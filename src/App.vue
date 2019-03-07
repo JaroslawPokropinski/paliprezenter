bc58daa088907625a4a0be14b295e1bf51e0e740
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2019-03-07 21:15:11 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Builder />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Builder from './components/Builder'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Builder
   }
 }
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+  background-color: #141d26;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/Builder.vue
@@ -0,0 +1,119 @@
+<template>
+    <div id="builder">
+        <input type="text" v-model="text" class="builder-input">
+        <input type="button" value="Add Male" v-on:click="onAddMale()" class="builder-button">
+        <input type="button" value="Add Female" v-on:click="onAddFemale()" class="builder-button">
+        <input type="button" value="Add Pause" v-on:click="onAddPause()" class="builder-button">
+        <input type="button" value="Play" v-on:click="onPlay()" class="builder-button">
+
+        <ul class="builder-list">
+            <li v-for="(m, index) in list" :key="m.id" class="builder-item"> {{ (m.pause) ? `Pause: ${m.timeout}ms` : `${m.voice}: ${m.text}` }} 
+                <span class="cancel-button" v-on:click="onDelete(index)">
+                    <img src="cancel.svg" width="16px" height="16px">
+                </span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'builder',
+    data () {
+        return {
+            count: 0,
+            text: '',
+            list: []
+        }
+    },
+    methods: {
+        onAddMale () {
+            if (this.text.length <= 0) {
+                return
+            }
+            this.list.push({id: this.count, text: this.text, voice: 'male'})
+            this.count ++
+            this.text = ''
+        },
+        onAddFemale () {
+            if (this.text.length <= 0) {
+                return
+            }
+            this.list.push({id: this.count, text: this.text, voice: 'female'})
+            this.count ++
+            this.text = ''
+        },
+        onAddPause () {
+            const timeout = parseInt(this.text)
+            if (timeout) {
+                this.list.push({id: this.count, pause: true, timeout})
+                this.count ++
+            }
+            this.text = ''
+        },
+        onPlay () {
+            if (this.list.length <= 0) {
+                return
+            }
+            if (this.list[0].pause) {
+                const l = this.list.shift()
+                setTimeout(() => this.onPlay(), l.timeout)
+                return
+            }
+            const voice = (this.list[0].voice === 'male') ? 'Polish Male' : 'Polish Female'
+            responsiveVoice.speak(this.list.shift().text, voice, {onend: () => {
+                this.onPlay()
+            }})
+        },
+        onDelete (index) {
+            this.list.splice(index, 1)
+        }
+    }
+}
+</script>
+
+<style>
+.builder-input {
+    font-family: 'Roboto', sans-serif;
+    display: block;
+    width: 100%;
+    box-sizing:border-box;
+    margin: 8px 0px;
+    padding: 1rem 1rem;
+    font-size: 2rem;
+    border: 0;
+    border-radius: 3px;
+    background: #334a66;
+}
+
+.builder-button {
+    font-family: 'Roboto', sans-serif;
+    background-color: #c51f5d;
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-decoration: none;
+    margin: 4px 2px;
+    cursor: pointer;
+    color: white;
+}
+
+.builder-list {
+    text-align: left;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #334a66;
+}
+
+.builder-item {
+    display: block;
+    color: white;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+.cancel-button {
+    float: right;
+}
+</style>
